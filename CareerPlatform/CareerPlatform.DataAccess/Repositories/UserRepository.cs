c42973c2b2bff7af0a1c26c5af63c56aa7952e05
karolisMtb <karolis.mtb@gmail.com>
2023-12-27 10:57:46 +0200

    Initial update

---
+++ CareerPlatform/CareerPlatform.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using CareerPlatform.DataAccess.DatabaseContext;
+using CareerPlatform.DataAccess.Entities;
+using CareerPlatform.DataAccess.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace CareerPlatform.DataAccess.Repositories
+{
+    public class UserRepository(ApplicationDbContext _platformDbContext, IUnitOfWork _unitOfWork, UserManager<User> _userManager) : IUserRepository
+    {
+
+
+
+        //public async Task<User> GetByEmailAddressAsync(string email)
+        //{
+        //    return await _platformDbContext.Users.Where(x => x.Email == email).FirstOrDefaultAsync();
+        //
+        //}
+
+
+
+        //public async Task<User> GetUserByIdAsync(Guid userId)
+        //{
+        //    User user = await _platformDbContext.Users.Where(u => u.Id == userId.ToString()).FirstAsync(); //prideta ToString
+        //    if (user == null)
+        //    {
+        //        throw new UserNotFoundException("User was not found.");
+        //    }
+        //
+        //    return user;
+        //}
+
+
+
+        //public async Task<User> GetUserByLoginCredentialsAsync(string credential)
+        //{
+        //    User currentUser = await _platformDbContext.Users.Where(x => x.UserName.Equals(credential) || x.Email.Equals(credential)).FirstAsync();
+        //
+        //    if (currentUser == null)
+        //    {
+        //        throw new UserNotFoundException($"User with login '{credential}' credential was not found.");
+        //    }
+        //    return currentUser;
+        //}
+        //
+        //
+    }
+}
